f110584edc10b9d24af24c58f641b36047a2d21a
nanjizhiyin <nanjizhiyin@163.com>
2018-11-15 15:50:15 +0800

    添加测试代码

--- src/main/java/com/xpfirst/hdrRouter/aop/Log.java
+++
@@ -1,7 +0,0 @@
-package com.xpfirst.hdrRouter.aop;
-
-/**
- * Created by mac on 2018/4/13.
- */
-public class Log {
-}
--- src/main/java/com/xpfirst/hdrRouter/aop/PointCuts.java
+++ src/main/java/com/xpfirst/hdrRouter/aop/PointCuts.java
@@ -3,7 +3,7 @@ package com.xpfirst.hdrRouter.aop;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class PointCuts {
-    @Pointcut(value = "within(com.xpfirst.hdrRouter.action.*)")
+    @Pointcut(value = "within(com.xpfirst.hdrRouter.service.UserService)")
     public void aopDemo() {
         System.out.print("fasdfasfafdasfd");
     }
